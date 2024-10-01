e24059767de9c2683d769f6eb0875b0e75bb4f57
MAC <mac@MACs-MacBook-Air.local>
2024-10-01 19:27:31 +0100

    GWG

---
+++ gwg.js
@@ -0,0 +1,6 @@
+function changeMenu() {
+  const menuToggle = document.querySelector(".menuToggle");
+  menuToggle.classList.toggle("active");
+  const navLinks = document.querySelector(".navLinks");
+  navLinks.classList.toggle("active");
+}
